f4925874696807229c45d73f0f1166d7ffa30abd
mohammadclashclash <mohammadclashclash@gmail.com>
2016-01-10 14:32:24 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -229,7 +229,7 @@ function create_config( )
     "version",
     "all"
     },
-    sudo_users = {150575718,0,tonumber(our_id)},--Sudo users
+    sudo_users = {150575718,104254582,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {150575718},--Realms Id
     moderation = {data = 'data/moderation.json'},
